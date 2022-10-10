f8449c4c4e101ae380367a084bdc3870c5b4261b
mukit24 <mukithasanpranto4@gmail.com>
2022-10-10 17:10:01 +0600

    simple_chaincode

--- fabcar/javascript/invoke.js
+++ fabcar/javascript/invoke.js
@@ -42,7 +42,7 @@ async function main() {
         // Submit the specified transaction.
         // createCar transaction - requires 5 argument, ex: ('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom')
         // changeCarOwner transaction - requires 2 args , ex: ('changeCarOwner', 'CAR12', 'Dave')
-        await contract.submitTransaction('createCar', 'CAR12', 'Honda', 'Accord', 'Black', 'Tom');
+        await contract.submitTransaction('writeDate', 'key1', 'have a nice day');
         console.log('Transaction has been submitted');
 
         // Disconnect from the gateway.
